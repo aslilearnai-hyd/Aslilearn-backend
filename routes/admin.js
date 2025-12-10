12f53b8fdf3552cd82a0c7baaf74eba4ed491b8d
Akhilesh2006s <akhileshsamayamanthula@gmail.com>
2025-12-10 23:56:00 +0530

    ok

--- routes/admin.js
+++ routes/admin.js
@@ -30,7 +30,6 @@ import {
   assignSubjects,
   assignClasses,
   getStudentAnalytics,
-  getIndividualStudentAnalytics,
   assignSubjectsToStudent,
   assignClassToStudent,
   assignSubjectsToClass,
@@ -76,7 +75,6 @@ router.get('/teacher/dashboard', getTeacherDashboardStats);
 // Student Management Routes
 router.get('/students', getStudents);
 router.get('/students/analytics', getStudentAnalytics);
-router.get('/students/:id/analytics', getIndividualStudentAnalytics);
 router.post('/students', addAdminIdToBody, createStudent);
 router.put('/students/:id', verifyDataOwnership(User), updateStudent);
 router.delete('/students/:id', verifyDataOwnership(User), deleteStudent);
